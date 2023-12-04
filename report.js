084c80a2be6cf65af0a04947616a8ba32652f55a
07Yuvrajtiwari <yuvrajtiwari.sage@gmail.com>
2023-12-04 10:20:26 +0530

    new changes got build pass

--- report.js
+++ report.js
@@ -9,4 +9,31 @@ const options = {
   launchReport: true,
 };
 
+
+
+
+const reporter = require('cucumber-html-reporter');
+const fs = require('fs');
+const jsonPath = 'cucumber-report.json';
+
+if (fs.existsSync(jsonPath)) {
+  console.log(`Found JSON file at ${jsonPath}`);
+  reporter.generate(options);
+  console.log('HTML report generated successfully.');
+
+  // Check if there are failures in the JSON file
+  const jsonData = require(jsonPath);
+  const hasFailures = jsonData[0].elements.some((element) =>
+    element.steps.some((step) => step.result.status === 'failed')
+  );
+
+  if (hasFailures) {
+    console.error('There are failed steps. Exiting with a non-zero status code.');
+    process.exit(1); // Exit with a non-zero status code indicating failure
+  }
+} else {
+  console.error(`Error: JSON file not found at ${jsonPath}`);
+  process.exit(1); // Exit with a non-zero status code indicating failure
+}
+
 reporter.generate(options);
